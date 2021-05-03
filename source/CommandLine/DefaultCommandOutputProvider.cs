bebd7310ce884eb1d4beb9b6c8708da400bec551
Matt Richardson <matt.richardson@octopus.com>
2021-05-03 12:16:34 +1000

    Better default implementation for GetAppVersion

--- source/CommandLine/DefaultCommandOutputProvider.cs
+++ source/CommandLine/DefaultCommandOutputProvider.cs
@@ -13,8 +13,13 @@ namespace Octopus.CommandLine
 
         protected override string GetAppVersion()
         {
-            var version = Assembly.GetEntryAssembly().GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion;
-            return version;
+            var entryAssembly = Assembly.GetEntryAssembly();
+            if (entryAssembly == null)
+                throw new ApplicationException("Unable to determine entry assembly");
+            var assemblyInformationalVersionAttribute = entryAssembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>();
+            if (assemblyInformationalVersionAttribute != null)
+                return assemblyInformationalVersionAttribute.InformationalVersion;
+            return entryAssembly.GetName().Version.ToString();
         }
 
         protected override string SerializeObjectToJson(object o)
