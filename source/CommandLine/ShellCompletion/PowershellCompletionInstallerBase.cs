d9d79f875cca4c147fe85b7c5de52593c9c0fe79
Matt Richardson <matt.richardson@octopus.com>
2021-05-03 15:54:05 +1000

    Fix up pwsh completer

--- source/CommandLine/ShellCompletion/PowershellCompletionInstallerBase.cs
+++ source/CommandLine/ShellCompletion/PowershellCompletionInstallerBase.cs
@@ -1,4 +1,6 @@
-
+using System;
+using System.Linq;
+using System.Text;
 using Octopus.CommandLine.Plumbing;
 
 namespace Octopus.CommandLine.ShellCompletion
@@ -8,14 +10,27 @@ namespace Octopus.CommandLine.ShellCompletion
         readonly string[] executableNames;
         protected static string PowershellProfileFilename => "Microsoft.PowerShell_profile.ps1";
 
-        public override string ProfileScript =>
-@"Register-ArgumentCompleter -Native -CommandName octo -ScriptBlock {
-    param($wordToComplete, $commandAst, $cursorPosition)
-    $parms = $commandAst.ToString().Split(' ') | select -skip 1
-    octo complete $parms | % {
-        [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterName', $_)
-    }
-}";
+        public override string ProfileScript
+        {
+            get
+            {
+                var results = new StringBuilder();
+
+                foreach (var command in executableNames.Distinct(StringComparer.OrdinalIgnoreCase))
+                {
+                    results.AppendLine($"Register-ArgumentCompleter -Native -CommandName {command} -ScriptBlock {{");
+                    results.AppendLine("    param($wordToComplete, $commandAst, $cursorPosition)");
+                    results.AppendLine("    $parms = $commandAst.ToString().Split(' ') | select -skip 1");
+                    results.AppendLine($"    {command} complete $parms | % {{");
+                    results.AppendLine("        [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterName', $_)");
+                    results.AppendLine("    }");
+                    results.AppendLine("}");
+                }
+
+                return results.ToString();
+            }
+        }
+
         public PowershellCompletionInstallerBase(ICommandOutputProvider commandOutputProvider, IOctopusFileSystem fileSystem, string[] executableNames)
             : base(commandOutputProvider, fileSystem, executableNames)
         {
