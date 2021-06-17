d0e33ee875afbbd9d84d7312441025a1d1aa0eb7
Matt Richardson <matt.richardson@octopus.com>
2021-06-18 05:57:07 +1000

    Fix namespace

--- source/CommandLine/OptionParsing/OptionAction.cs
+++ source/CommandLine/OptionParsing/OptionAction.cs
@@ -1,4 +1,6 @@
-﻿namespace Octopus.CommandLine
+﻿using System;
+
+namespace Octopus.CommandLine.OptionParsing
 {
     public delegate void OptionAction<in TKey, in TValue>(TKey key, TValue value);
 }
